38d3f71462656f2a8307a539a8b19b62fe078d35
Miahri <mahrihommadova@gmail.com>
2023-01-07 00:39:40 +0300

    Added social media layout and grid styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export function App() {
+    return (
+        <div className='app-wrapper'>
+            <header className='app-header'>
+                <img src="https://img.freepik.com/premium-vector/illustration-concept-of-samurai-warrior_157713-245.jpg?w=2000"/>
+            </header>
+            <nav className='app-nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>Photos</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='app-content'>
+                <div className='ava'>
+                    <img src="https://production-it-incubator.s3.eu-central-1.amazonaws.com/personal-page-front-public%3Aavatar/Image/fe6760cb-5e82-453b-a3d2-7fe1b1f70b00_photo_2023-01-02_15-52-26.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
