f51f43130160ebb3aa1e36b046dc4d0a1fa7b32f
Miahri <mahrihommadova@gmail.com>
2023-08-27 19:42:16 +0300

    added input component to login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Field, reduxForm} from "redux-form";
 import {required} from "../../utils/validators/validators";
+import {Input} from "../../common/FormsControls/FormsControls";
 
 type LoginFormType = {
   onSubmit: () => void
@@ -24,13 +25,13 @@ const LoginForm = (props: any) => {
   return (
     <form onSubmit={props.onSubmit}>
       <div>
-        <Field validate={[required]} placeholder={"Login"} name={'login'} component={"input"}></Field>
+        <Field validate={[required]} placeholder={"Login"} name={'login'} component={Input}></Field>
       </div>
       <div>
-        <Field validate={[required]} placeholder={"Password"} name={'password'} component={"input"}></Field>
+        <Field validate={[required]} placeholder={"Password"} name={'password'} component={Input}></Field>
       </div>
       <div>
-        <Field validate={[required]} name={'rememberMe'} component={"input"} type={"checkbox"}>remember me</Field>
+        <Field validate={[required]} name={'rememberMe'} component={Input} type={"checkbox"}>remember me</Field>
       </div>
       <div>
         <button>Login</button>
