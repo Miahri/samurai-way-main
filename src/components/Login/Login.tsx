139d3f64331daa597de89c51d80527553149dbc7
Miahri <mahrihommadova@gmail.com>
2023-09-02 20:30:16 +0300

    refactoring of Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import {Field, reduxForm} from "redux-form";
 import {required} from "../../utils/validators/validators";
 import {Input} from "../../common/FormsControls/FormsControls";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {AppRootStateType} from "../../redux/redux-store";
+import {Redirect} from "react-router-dom";
 
-type LoginFormType = {
-  onSubmit: () => void
+type LoginPropsType = {
+  isAuth: boolean
+  login: (email: string, password: string, rememberMe: boolean) => void
 }
 
-const Login = () => {
+const mapStateToProps = (state: AppRootStateType) => {
+  return {
+    isAuth: state.authPage.isAuth
+  }
+}
+
+const Login = (props: LoginPropsType) => {
   const onSubmit = (formData: any) => {
-    console.log(formData)
+    props.login(formData.email, formData.password, formData.rememberMe);
+  }
+
+  if (props.isAuth) {
+    return <Redirect to={'/profile'} />
   }
 
   return (
-
     <div>
       <h1>Login</h1>
       <LoginReduxForm onSubmit={onSubmit}/>
@@ -42,4 +56,4 @@ const LoginForm = (props: any) => {
 
 const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
-export default Login;
+export default connect(mapStateToProps, {login})(Login);
