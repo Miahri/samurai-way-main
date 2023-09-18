326351e9f6f5b3f168dd01d1809b94457b754f39
Miahri <mahrihommadova@gmail.com>
2023-09-18 21:36:16 +0300

    addedt test 'new post should be added with correct message title' to profile-reducer

--- src/test/profile-reducer.ts
+++ src/test/profile-reducer.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import {addPostActionCreator, profileReducer} from "../redux/profile-reducer";
 
 it('new post should be added', () => {
@@ -15,4 +14,20 @@ it('new post should be added', () => {
   let newState = profileReducer(state, action);
 
   expect(newState.posts.length).toBe(3);
+});
+
+it('new post should be added with correct message title', () => {
+  let action = addPostActionCreator("My 3d post");
+  let state = {
+    posts: [
+      {message: "Hi. How are you?", likesCount: 15},
+      {message: "It's my first post here!", likesCount: 20}
+    ],
+    profile: null,
+    status: ''
+  };
+
+  let newState = profileReducer(state, action);
+
+  expect(newState.posts[3].message).toBe("My 3d post");
 });
