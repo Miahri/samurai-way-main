2e49779dac26ace6b98a6d8c54495ecc7a7f34a7
Miahri <mahrihommadova@gmail.com>
2023-09-26 23:33:16 +0300

    refactoring Dialogs with createField function and added typization

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,9 +3,9 @@ import dialogsModule from './Dialogs.module.css';
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {DialogsType, MessagesType} from "../../redux/state";
-import {Field, reduxForm} from "redux-form";
+import {InjectedFormProps, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../utils/validators/validators";
-import {Textarea} from "../../common/FormsControls/FormsControls";
+import {createField, Textarea} from "../../common/FormsControls/FormsControls";
 
 type DialogsPropsType = {
   newMessageText: string
@@ -15,13 +15,17 @@ type DialogsPropsType = {
   sendMessage: (values: any) => void
 };
 
+type FormDataType = {
+  newMessageBody: string
+}
+
 const maxLength50 = maxLengthCreator(50);
 
 export const Dialogs = (props: DialogsPropsType) => {
   let dialogItems = props.dialogs.map((d: DialogsType) => <DialogItem id={d.id} name={d.name}/>);
   let messageItems = props.messages.map((m: MessagesType) => <Message message={m.message}/>);
 
-  const sendMessage = (values: any) => {
+  const sendMessage = (values: FormDataType) => {
     props.sendMessage(values.newMessageBody)
   };
 
@@ -38,13 +42,10 @@ export const Dialogs = (props: DialogsPropsType) => {
   )
 };
 
-const AddMessageForm = (props: any) => {
+const AddMessageForm = (props: InjectedFormProps<FormDataType>) => {
   return (
     <form onSubmit={props.handleSubmit}>
-      <div>
-        <Field validate={[required, maxLength50]}
-               component={Textarea} name="newMessageBody" placeholder="Enter your message"/>
-      </div>
+      {createField([required, maxLength50], "Enter your message", "newMessageBody", Textarea)}
       <div>
         <button>Send message</button>
       </div>
@@ -52,4 +53,4 @@ const AddMessageForm = (props: any) => {
   )
 };
 
-const AddMessageFormRedux = reduxForm({form: 'DialogAddMessageForm'})(AddMessageForm);
+const AddMessageFormRedux = reduxForm<FormDataType>({form: 'DialogAddMessageForm'})(AddMessageForm);
