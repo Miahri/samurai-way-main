3eb9453afc8fb3d42a1a1cb925ea6c5ada8c872d
Miahri <mahrihommadova@gmail.com>
2023-09-27 20:22:16 +0300

    added react-test-renderer library

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
@@ -8,6 +8,12 @@ import thunkMiddleware from "redux-thunk";
 import {reducer as formReducer} from "redux-form";
 import {appReducer} from "./app-reducer";
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const rootReducer = combineReducers({
   profilePage: profileReducer,
   dialogPage: dialogsReducer,
@@ -16,11 +22,12 @@ const rootReducer = combineReducers({
   authPage: authReducer,
   app: appReducer,
   form: formReducer
-})
+});
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof rootReducer>;
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const composeEnhance = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer, composeEnhance(applyMiddleware(thunkMiddleware)));
 
 //@ts-ignore
 window.store = store;
