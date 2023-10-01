c0b812906c3836991939e8bcd8ebce4833b538fe
Miahri <mahrihommadova@gmail.com>
2023-10-01 22:24:16 +0300

    added test for profileStatus component the  displaying of input el instead of span

--- src/test/ProfileStatus.test.tsx
+++ src/test/ProfileStatus.test.tsx
@@ -22,4 +22,12 @@ describe("ProfileStatus component", () => {
       let input = root.findByType("input");
     }).toThrow();
   });
+  test("input should be displayed in editMode instead of span", () => {
+    const component = create(<ProfileStatus status={"it-kamasutra.com"} updateStatus={() => {}}/>);
+    const root = component.root;
+    let span = root.findByType("span");
+    span.props.onDoubleClick();
+    let input = root.findByType("input");
+    expect(input.props.value).toBe("it-kamasutra.com");
+  });
 })
