741040cf5510a02acee325a1d93e2c74282a5bde
Miahri <mahrihommadova@gmail.com>
2023-10-02 22:31:16 +0300

    added test for profileStatus component "callback should be called"

--- src/test/ProfileStatus.test.tsx
+++ src/test/ProfileStatus.test.tsx
@@ -30,4 +30,11 @@ describe("ProfileStatus component", () => {
     let input = root.findByType("input");
     expect(input.props.value).toBe("it-kamasutra.com");
   });
+  test("callback should be called", () => {
+    const mockCallback = jest.fn();
+    const component = create(<ProfileStatus status={"it-kamasutra.com"} updateStatus={mockCallback}/>);
+    const instance = component.getInstance()?.instance;
+    instance.deactivateEditMode();
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
 })
